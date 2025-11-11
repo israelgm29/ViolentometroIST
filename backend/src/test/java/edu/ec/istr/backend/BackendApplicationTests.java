e3d1e5173408c4cde4214c40707c6e1a52220aad
Juan Carlos Reinoso Loarte <jucareinosoloarte@gmail.com>
2025-11-10 23:54:36 -0500

    Creacion del proyecto en su estructura global

---
+++ backend/src/test/java/edu/ec/istr/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ec.istr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
