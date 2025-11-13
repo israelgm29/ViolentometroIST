da46f9ffae3590220cc10898673372d0ea126c88
Juan Carlos Reinoso Loarte <jucareinosoloarte@gmail.com>
2025-11-13 10:20:04 -0500

    Cambios de configuracion de frontend

--- frontend/violentometro/capacitor.config.ts
+++ frontend/violentometro/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.violentometro.app',
   appName: 'Violentómetro',
-  webDir: 'www/browser',  // RUTA CORRECTA
+  webDir: 'www/browser',
   server: {
     androidScheme: 'https'
   }
